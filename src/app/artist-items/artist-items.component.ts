78a346a63c8e205a13168aa31b1ce48119b04064
Daryl <daryl@briviant.com>
2019-07-04 05:08:19 -0700

    added inputs as artist

--- src/app/artist-items/artist-items.component.ts
+++ src/app/artist-items/artist-items.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-artist-items',
-  templateUrl: './artist-items.component.html'
+  templateUrl: './artist-items.component.html',
+  inputs: ['artist']
 })
 export class ArtistItemsComponent implements OnInit {
 
